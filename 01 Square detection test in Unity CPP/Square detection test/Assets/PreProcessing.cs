fdc62d9e89201d2cc3866b9c9a7d9090235feacc
naberg14 <emil.digit@gmail.com>
2015-10-21 15:59:42 +0200

    Created new folder for CCP

---
+++ 01 Square detection test in Unity CPP/Square detection test/Assets/PreProcessing.cs
@@ -0,0 +1,302 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class PreProcessing : Singleton<PreProcessing> {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float[] BubbleSort(float[] input) {
+		for (int j = 0; j < input.Length; j++) {
+			for (int i = 0; i < input.Length-1-j; i++) {
+				float tmp;
+				if (input [i] < input [i + 1]) {
+					tmp = input [i];
+					input [i] = input [i + 1];
+					input [i + 1] = tmp;
+				}
+			}
+			
+		}
+		
+		return input;
+	}
+
+	public int[] BubbleSort(int[] input) {
+		for (int j = 0; j < input.Length; j++) {
+			for (int i = 0; i < input.Length-1-j; i++) {
+				int tmp;
+				if (input [i] < input [i + 1]) {
+					tmp = input [i];
+					input [i] = input [i + 1];
+					input [i + 1] = tmp;
+				}
+			}
+			
+		}
+		
+		return input;
+	}
+	
+	public Color[,] Threshold (Color[,] i, float t)
+	{
+		for (int w = 0; w < i.GetLength(0); w++) {
+			for (int h = 0; h < i.GetLength(1); h++) {
+				i [w, h].r = i [w, h].r < t ? 0f : 1f;
+				i [w, h].g = i [w, h].g < t ? 0f : 1f;
+				i [w, h].b = i [w, h].b < t ? 0f : 1f;
+			}
+		}
+		return i;
+	}
+
+	public Color[,] Rank (Color[,] i, int k, int t)
+	{
+		
+		Color[,] tmp = i;
+		
+		int kw = k / 2;
+		int kh = k / 2;
+
+		//Double for loop through the pixels of the input image
+		
+		for (int w = kw; w < i.GetLength(0) - kw; w++) {
+			for (int h = kh; h < i.GetLength(1) - kh; h++) {
+
+				// Integer to be interated
+
+				int count = 0;
+				
+				float [] neighborhood = new float[k * k];
+				
+				for (int j = -kw; j <= kw; j++) {
+					for (int l = -kh; l <= kh; l++) {
+						
+						neighborhood [count] = (tmp [w + j, h + l].r + tmp [w + j, h + l].g + tmp [w + j, h + l].b) / 3;
+						count ++;
+					}
+				}
+				
+				float result = 0f;
+				
+				switch (t) {
+				case 1: //median
+					neighborhood = BubbleSort (neighborhood);
+					result = neighborhood [neighborhood.Length / 2];
+					break;
+				case 2: // difference
+					result = neighborhood.Max () - neighborhood.Min ();
+					break;
+				case 3:
+					neighborhood = BubbleSort (neighborhood);
+					if (neighborhood[neighborhood.Length] >= 1) {
+						result = 1;
+
+					} else {
+						result = 0;
+					}
+
+
+
+
+					break;
+				default:
+					result = 0.5f;
+					break;
+				}
+				
+				
+				
+				i [w, h].r = result;
+				i [w, h].g = result;
+				i [w, h].b = result;
+			}
+		}
+		return i;
+	}
+
+	public Color[,] Rgb2greyScale (Color[,] i){
+		for (int w = 0; w < i.GetLength(0); w++) {
+			for (int h = 0; h < i.GetLength(1); h++) {
+				float grey = (i [w, h].r + i [w, h].g + i [w, h].b) / 3;
+				i [w, h].r = grey;
+				i [w, h].g = grey;
+				i [w, h].b = grey;
+			}
+		}
+		return i;
+	}
+
+	public Color [,] simpleBrightness(Color [,] i, int b){
+		float c = b / 255f;
+		for(int w = 0; w < i.GetLength(0); w++){
+			for(int h = 0; h < i.GetLength(1); h++){
+				
+				i[w,h].r += c;
+				i[w,h].g += c;
+				i[w,h].b += c;
+				
+			}
+		}
+		return i;
+	}
+	public Color [,] simpleContrast(Color [,] i, float b){
+		for(int w = 0; w < i.GetLength(0); w++){
+			for(int h = 0; h < i.GetLength(1); h++){
+				
+				i[w,h].r *= b;
+				i[w,h].g *= b;
+				i[w,h].b *= b;
+				
+			}
+		}
+		return i;
+	}
+
+	public void printSample (Color [,] i, int x, int y, int huh){
+
+
+		for (int w = x; w < x+huh; w++) {
+			for (int h = y; h < y+huh; h++) {
+
+				print (i[w,h]);
+
+			}
+		}
+
+
+	}
+
+	public Color[,] Erode (Color[,] i) {
+
+		Color[,] tmp = i;
+
+		Color[,] result = new Color[i.GetLength(0), i.GetLength(1)];
+
+
+		for (int w = 3; w < i.GetLength(0) - 3; w++) {
+			for (int h = 3; h < i.GetLength(1) - 3; h++) {
+				
+				int count = 0;
+				float sum = 0;
+				
+				int [] neighborhood = new int[49];
+				
+				for (int j = -3; j <= 3; j++) {
+					for (int l = -3; l <= 3; l++) {
+
+						sum += (tmp [w + j, h + l].r);
+
+						count ++;
+
+					}
+				}
+			
+
+
+			
+				if (sum <= 0f) {
+					result[w,h].r = 0f;
+					result[w,h].g = 0f;
+					result[w,h].b = 0f;
+				}else {
+					result[w,h].r = 1f;
+					result[w,h].g = 1f;
+					result[w,h].b = 1f;
+				}
+
+
+			}
+		}
+
+
+
+		return result;
+	}
+
+	public Color[,] Dilate (Color[,] i) {
+		
+		Color[,] tmp = i;
+		
+		Color[,] result = new Color[i.GetLength(0), i.GetLength(1)];
+		
+		
+		for (int w = 3; w < i.GetLength(0) - 3; w++) {
+			for (int h = 3; h < i.GetLength(1) - 3; h++) {
+				
+				int count = 0;
+				float sum = 0;
+				
+				int [] neighborhood = new int[49];
+				
+				for (int j = -3; j <= 3; j++) {
+					for (int l = -3; l <= 3; l++) {
+						
+						sum += (tmp [w + j, h + l].r);
+						
+						count ++;
+						
+					}
+				}
+				
+				
+				
+				
+				if (sum < 49f) {
+					result[w,h].r = 0f;
+					result[w,h].g = 0f;
+					result[w,h].b = 0f;
+				}else {
+					result[w,h].r = 1f;
+					result[w,h].g = 1f;
+					result[w,h].b = 1f;
+				}
+				
+				
+			}
+		}
+		
+		
+		
+		return result;
+	}
+
+	public Color[,] GetPixels2D (Texture2D i)
+	{
+		
+		Color[,] texture2D = new Color[i.width, i.height];
+		Color[] texture1D = i.GetPixels ();
+		
+		for (int h = 0; h < i.height; h++) {
+			for (int w = 0; w < i.width; w++) {
+				texture2D [w, h] = texture1D [h * i.width + w];
+			}
+		}
+		return texture2D;
+	}
+	
+	public void SetPixels2D (Color[,] i, Texture2D t)
+	{
+		Color[] texture1D = new Color[i.Length];
+
+		int width = i.GetLength (0);
+		int height = i.GetLength (1);
+		
+		for (int h = 0; h < height; h++) {
+			for (int w = 0; w < width; w++) {
+				texture1D [h * width + w] = i [w, h]; 
+			}
+		}
+		
+		t.SetPixels (texture1D);
+		t.Apply ();
+	}
+}
---
+++ 01 Square detection test in Unity CPP/Square detection test/Assets/squareDetection.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class squareDetection : MonoBehaviour
+{
+
+	
+	/*
+ - We're importing a texture (image), its public so that we can input any picure.
+ - We create a new texture whihch is empty.
+ - We create an array of Colors, which holds the RGB values for each pixel
+ - We create a 2D array of integers that will hold the greyscale from Color
+ - We create another 2D array which is to hold all the tag-values of the pixels
+ - We create min and max values for detected rectangles
+ - We create a integer that will hold the currect tag number
+ - We create a CubeCorner array to hold any amount of rectangle objects to be detected
+*/
+	public Texture2D inputTex;
+	private Texture2D texture;
+	Color[] pix;
+	Color[,] image;
+	int[,] positionValue;
+	int[,] tags;
+	int xmin, xmax, ymin, ymax;
+	int tagCheck = 0;
+	CubeCorner[] cubecorners;
+	
+	void Start ()
+	{
+		
+		/*
+		 - We set the empty texture to the inputTex (this might create a copy by reference problem)
+		 - We set the renderer of the gameobject this script is attached to, to the texture (input texture)
+		 - We set the pix array of Colors to the texture of the texture (input texture)
+		*/
+
+		texture = new Texture2D (inputTex.width, inputTex.height);
+
+		Color[,] image = PreProcessing.Instance.GetPixels2D (inputTex);
+
+
+
+		image = PreProcessing.Instance.Rgb2greyScale (image);
+
+		image = PreProcessing.Instance.simpleBrightness (image, -30);
+
+		image = PreProcessing.Instance.simpleContrast (image, 1.3f);
+
+
+		image = PreProcessing.Instance.Rank (image, 3, 1);
+
+		
+		image = PreProcessing.Instance.Threshold (image, 0.8f);
+
+
+
+		//PreProcessing.Instance.printSample (image, 300,50,10);
+
+		//image = PreProcessing.Instance.Dilate (image);
+		//image = PreProcessing.Instance.Dilate (image);
+		//image = PreProcessing.Instance.Dilate (image);
+
+
+
+
+		PreProcessing.Instance.SetPixels2D (image, texture);
+
+
+
+		this.GetComponent<Renderer> ().material.mainTexture = texture;
+		pix = texture.GetPixels ();
+		
+		/*
+		- Both of our 2D arrays of integers take the height and width of the image pixels
+		- The array of tags gets 10 pixels worth of padding, otherwise it will go out of bounds later
+		*/
+		
+		positionValue = new int[texture.width, texture.height];
+		tags = new int[texture.width + 1, texture.height + 1];
+		
+		
+		/*
+		- We loop through the width and height of the image
+		- We set the position values to the greyscale of the image, 0-255
+		- We set all tags to 0
+		*/
+		
+		for (int i = 1; i < texture.width; i++) {
+			for (int j = 1; j < texture.height; j++) {
+				positionValue [i, j] = (int)(pix [i + (pix.Length / texture.height * j)].grayscale * 255);
+				tags [i, j] = 0;
+				
+				/*
+		- If a pixel is not white (some thresholding has to be done before this)
+		*/
+				if (positionValue [i, j] < 255) {
+					/*
+		- We loop through a 3*3 kernel, which is applied to the pixel which was not white
+		*/
+					for (int ky=-1; ky <= 1; ky++) {
+						for (int kx=-1; kx <= 1; kx++) {
+							/*
+		- If any of the 9 pixels from the kernel has a tag
+		*/
+							if (tags [i + ky, j + kx] != 0) {
+								/*
+		- We then tag our current non-white pixel with the same tag
+		*/
+								tags [i, j] = tags [i + ky, j + kx];
+							}
+						}
+					}
+					
+					
+					/*
+		- If none of the neighbours has a tag, we create a new one by incrementing tagCheck
+		- And we then tag our non-white pixel with the new tag
+		*/
+					if (tags [i, j] == 0) {
+
+						tags [i, j] = tagCheck;
+						tagCheck++;
+					}
+				}
+			}
+		}
+		
+		/*
+		- We set an array of CubeCorners to the size of TagCheck, which is the total amount of objects
+		- We set the values of all corners to their theoretical maximum value, either 0 or the size of the input image
+		*/
+		
+		cubecorners = new CubeCorner[tagCheck];
+
+		for (int k = 0; k < tagCheck; k++) {
+			cubecorners[k] = new CubeCorner();
+		}
+
+		for (int k = 0; k < tagCheck; k++) {
+
+			cubecorners [k].xmin = texture.width;
+			cubecorners [k].xmax = 0;
+			cubecorners [k].ymin = texture.height;
+			cubecorners [k].ymax = 0;
+
+		}
+		
+		/*
+		- Loop through all the pixels
+		- Loop through the cube-objects
+		*/
+		
+		for (int i = 1; i < texture.width; i++) {
+			for (int j = 1; j < texture.height; j++) {
+				for (int k = 1; k < cubecorners.Length; k++) {
+					
+					/*
+		- I ask - If there is something tagged as k (0 -> amount of objects), 
+		- then set the corresponding cubecorner (k) values:
+		- if the i or j is lower or higher than the min or max of the object
+
+		- This will create values for all four corner of a a rectangle of any detected object
+		- Ready to be created in to a cube later on
+		*/
+					if (tags [i, j] == k && i < cubecorners [k].xmin) {
+						cubecorners [k].xmin = i;
+					}
+					if (tags [i, j] == k && i > cubecorners [k].xmax) {
+						cubecorners [k].xmax = i;
+					}
+					if (tags [i, j] == k && j < cubecorners [k].ymin) {
+						cubecorners [k].ymin = j;
+					}
+					if (tags [i, j] == k && j > cubecorners [k].ymax) {
+						cubecorners [k].ymax = j;
+					}
+				}
+			}
+		}
+
+		/*for (int i = 1; i < texture.width; i++) {
+			for (int j = 1; j < texture.height; j++) {
+				for (int k = 1; k < cubecorners.Length; k++) {
+
+					}
+					if (tags [i, j] == k && j < cubecorners [k].ymin) {
+						cubecorners [k].ymin = j;
+					}
+					if (tags [i, j] == k && j > cubecorners [k].ymax) {
+						cubecorners [k].ymax = j;
+					}
+				}
+			}
+		}*/
+		
+		/*
+		 *This is a test 
+		 *to check the numbers of all the cubes
+		 */
+		for (int k = 1; k < cubecorners.Length; k++) {
+			print ("Cube " + k + " xmin value is: " + cubecorners [k].xmin);
+			print ("Cube " + k + " xmax value is: " + cubecorners [k].xmax);
+			print ("Cube " + k + " ymin value is: " + cubecorners [k].ymin);
+			print ("Cube " + k + " ymax value is: " + cubecorners [k].ymax);
+
+			//starting to create cubes
+
+			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+
+			cube.transform.position = new Vector3 ((cubecorners [k].xmax + cubecorners [k].xmin) / 2, 0, (cubecorners [k].ymax + cubecorners [k].ymin) / 2);
+			cube.transform.localScale = new Vector3 (cubecorners [k].xmax - cubecorners [k].xmin, 1, cubecorners [k].ymax - cubecorners [k].ymin);
+			cube.AddComponent<Rigidbody> ().useGravity = false;
+			cube.GetComponent<Rigidbody> ().isKinematic = true;
+				
+			Color newColor = new Color ();
+			newColor = new Color (Random.value, Random.value, Random.value, 1.0f);
+			cube.GetComponent<Renderer> ().material.color = newColor;  //Will give cubes color after merging
+			cube.tag = "Ground";
+
+
+		}		
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		
+		/*
+ - This is a test, the test will print the following if and only if the image has 7 objects
+*/
+		/*if (tagCheck == 7) {
+			print ("tagchch" + tagCheck);
+		}*/
+	}
+}
+/*
+ *A CubeCorner Class, only used to store the four corners of a rectangle, to be come a cube 
+ */
+
+public class CubeCorner{
+	public int xmin, xmax, ymin, ymax;
+	
+	void setXmin(int i){
+		this.xmin = i;
+	}
+	void setXmax(int i){
+		this.xmax = i;
+	}
+	void setYmin(int i){
+		this.ymin = i;
+	}
+	void setYmax(int i){
+		this.ymax = i;
+	}
+	int getXmin(){
+		return xmin;
+	}
+	int getXmax(){
+		return xmax;
+	}
+	int getYmin(){
+		return ymin;
+	}
+	int getYmax(){
+		return ymax;
+	}
+
+}
+
